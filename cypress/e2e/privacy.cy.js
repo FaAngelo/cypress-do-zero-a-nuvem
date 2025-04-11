3c0f017e20f17a01f480ac1620732eb7f6993426
Fabiola Ângelo <Sfabiola012angelo@gmail.com>
2025-04-11 10:23:41 -0300

    Avançando no uso do cypress - Lodash

--- cypress/e2e/privacy.cy.js
+++ cypress/e2e/privacy.cy.js
@@ -1,8 +1,10 @@
 /// <reference types="cypress"/>
 
-describe('Política de privacidade', () => {
-    it('testa a política de privacidade de forma independente',() => {
-        cy.visit('/src/privacy.html')
-        cy.contains('h1','CAC TAT - Política de Privacidade').should('be.visible')
+Cypress._.times(5,()=>{
+    describe('Política de privacidade', () => {
+        it('testa a política de privacidade de forma independente',() => {
+            cy.visit('/src/privacy.html')
+            cy.contains('h1','CAC TAT - Política de Privacidade').should('be.visible')
+        })
     })
 })
